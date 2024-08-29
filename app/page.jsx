443143d2a4e9037d71ac521ca1b18f01cab8a4ae
zakerous <m.z.humayoon@gmail.com>
2024-08-29 13:09:19 +0300

    basic homepage

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,18 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text_center">
+            Discover and Share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient"> AI-Powered Prompt</span>
+        </h1>
+        <p className="desc text-center">
+        PromptLand is an open-source AI tool designed for the modern world,
+        enabling users to discover, create, and share imaginative prompts
+        </p>
+        {/* Feed */}
+    </section>
   )
 }
 
