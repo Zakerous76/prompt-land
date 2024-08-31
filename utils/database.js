78adfee6c40d3fdfe3f4a35392265b93940fb9ed
zakerous <m.z.humayoon@gmail.com>
2024-08-31 09:35:53 +0300

    moved dbName to .env

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "prompts",
+      dbName: process.env.DATABASE_NAME,
       useNewUrlParser: true,
       useUnifiedTopolgy: true,
     });
