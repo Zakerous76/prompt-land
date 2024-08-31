316f8cabae6700ad554a67d0a9692ba85f616000
zakerous <m.z.humayoon@gmail.com>
2024-08-31 11:20:42 +0300

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // additional checks to adhere to best practices
+  reactStrictMode: true,
+
+  // to be able to access .env from the client-side (browser)
+  // be sensitive, remove when not needed
+  env: {
+    dbName: process.env.DATABASE_NAME,
+  },
+
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        // edit this for security and performance considerations
+        hostname: "**",
+      },
+    ],
+  },
+
+  // expoerimental features in Next.js
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+
+  // customize webpack custmization
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+
+      // the ability to use "await" at the top of your modules
+      // without the need to wrap it in an async
+      topLevelAwait: true,
+    };
+    return config;
   },
 };
 
