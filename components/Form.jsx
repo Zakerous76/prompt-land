fd509f5d8b355dc2017f2314e1848a606a9123db
zakerous <m.z.humayoon@gmail.com>
2024-09-01 23:39:10 +0300

    Form UI

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmitting }) => {
   return (
-    <div>Form</div>
-  )
-}
+    <section className="w-full max-w-full md:max-w-screen-md flex-start flex-col">
+      <h1 className="head_text text-left blue_gradient">{type} Post</h1>
+      <p className="desc text-left max-w-md">
+        {type} and Share your amazing prompts with the wide wide world
+      </p>
 
-export default Form
+      <form
+        onSubmit={handleSubmitting}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.prompt })}
+            placeholder="Write your prompt here"
+            required
+            className="form_textarea"
+          />
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag <span className="font-normal">(#product #webdev #meme)</span>
+          </span>
+
+          <textarea
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.tag })}
+            placeholder="#tag"
+            required
+            className="form_input"
+          />
+        </label>
+        <div className="flex-end mx-3 mb-5 gap-5">
+          <Link
+            href="/"
+            className="text-gray-500 px-5 py-1.5  hover:font-bold transition-all"
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            disabled={submitting} // :bool
+            className="hover:font-bold transition-all ease-in-out px-5 py-1.5 rounded-full bg-primary-orange text-white "
+          >
+            {submitting ? `${type}...` : type}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
