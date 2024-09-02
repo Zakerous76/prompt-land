63820f8008044998b409c5d870b5eba001f996a2
zakerous <m.z.humayoon@gmail.com>
2024-09-02 10:40:58 +0300

    handled submission to the router (backend)

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,15 +5,43 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  // These state must be defined before starting to work on the Form componment
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
-
   const [submitting, setSubmitting] = useState(false);
 
-  const CreatePrompt = async (e) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const createPrompt = async (e) => {
     // fill later
+    // prevent reloadıng
+    e.preventDefault();
+    // to be used as a loader later on
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+      console.log(response);
+    } finally {
+      // either way do this:
+      setSubmitting(false);
+    }
   };
 
   return (
@@ -22,7 +50,7 @@ const CreatePrompt = () => {
       post={post}
       setPost={setPost}
       submitting={submitting}
-      handleSubmitting={CreatePrompt}
+      handleSubmitting={createPrompt}
       s
     />
   );
