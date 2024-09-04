4386168a32f7a823b8d9cb8849c87c512254f89a
zakerous <m.z.humayoon@gmail.com>
2024-09-04 09:14:00 +0300

    fully implemented

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+"use client";
 
-const PromptCard = () => {
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
+
+const PromptCard = ({ prompt, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState("");
+  const handleCopy = () => {
+    setCopied(prompt.prompt);
+    navigator.clipboard.writeText(prompt.prompt);
+    // after 3 secs, the copied state is reset, which will reset the copy icon
+    setTimeout(() => setCopied(""), 3000);
+  };
   return (
-    <div>PromptCard</div>
-  )
-}
+    <div className="prompt_card">
+      <div className="flex-1 flex justify-between items-start gap-5">
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={prompt.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className=""
+          />
 
-export default PromptCard
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">
+              {prompt.creator.username}
+            </h3>
+            <p className="font-inter text-sm text-gray-500">
+              {prompt.creator.email}
+            </p>
+          </div>
+        </div>
+        <div className="copy_btn transition-all" onClick={handleCopy}>
+          <Image
+            src={
+              copied === prompt.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+      <div>
+        <p className="font-satoshi text-sm text-gray-700 my-7 text-justify">
+          {prompt.prompt}
+        </p>
+        <p
+          className="font-inter text-sm blue_gradient cursor-pointer"
+          onClick={() => handleTagClick && handleTagClick(prompt.tag)}
+        >
+          {prompt.tag}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PromptCard;
