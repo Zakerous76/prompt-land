6a865f638f6b2349e47fbc15017ea29b37cc320b
zakerous <m.z.humayoon@gmail.com>
2024-09-04 09:20:21 +0300

    fetch prompts and create a card for each

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+// search bar
+// get prompts
+// display prompts
+
+// A component that is only used here
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {console.log("data: ", data)}
+      {data.map((prompt) => (
+        <PromptCard
+          key={prompt._id}
+          prompt={prompt}
+          handleTagClick={handleTagClick}
+        ></PromptCard>
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const [searchText, setSearchText] = useState("");
+  const handleSearchChange = (e) => {};
 
-export default Feed
+  const [prompts, setPrompts] = useState([]);
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      try {
+        const response = await fetch("/api/prompt");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setPrompts(data);
+        console.log("Fetched Prompts: ", data); // Log the fetched data
+      } catch (error) {
+        console.error("Failed to fetch prompts:", error);
+      }
+    };
+    fetchPrompts();
+  }, []);
+
+  // To log updated prompts
+  useEffect(() => {
+    console.log("Updated Prompts: ", prompts); // Log the updated prompts
+  }, [prompts]);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a prompt..."
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={prompts} handleTagClick={() => {}} />
+    </section>
+  );
+};
+
+export default Feed;
