c6200eaea7b9d73438a7310850f9ea08425fec1e
zakerous <m.z.humayoon@gmail.com>
2024-09-06 00:09:26 +0300

    did all endponits

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -36,3 +36,13 @@ export const PATCH = async (request, { params }) => {
   }
 };
 // DELETE (delete)
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectToDB();
+    await Prompt.findByIdAndDelete(params.id);
+    return new Response("Prompt deleted successfully", { status: 200 });
+  } catch (error) {
+    console.log("Failed to delete prompt:", error);
+    return new Response("Failed to delete prompt", { status: 500 });
+  }
+};
