b9f1125626517737bb1e6c152996e71374e5f65a
zakerous <m.z.humayoon@gmail.com>
2024-09-06 00:05:57 +0300

    the endpoint for prompts

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -12,11 +12,27 @@ export const GET = async (request, { params }) => {
     }
     return new Response(JSON.stringify(prompt), { status: 200 });
   } catch (error) {
-    console.log("Error in fetching prompts: ", error);
-    return new Response("Failed to fetch all prompts", { status: 500 });
+    console.log("Error in fetching the prompt: ", error);
+    return new Response("Failed to fetch prompt", { status: 500 });
   }
 };
 
 // PATCH (update)
-export const PATCH = (request, { params }) => {};
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+  try {
+    await connectToDB();
+    const existingPrompt = await Prompt.findById(params.id);
+    if (!existingPrompt)
+      return new Response("Prompt not found", { status: 404 });
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+    console.log("Failed to update prompt:", error);
+    return new Response("Failed to update prompt", { status: 500 });
+  }
+};
 // DELETE (delete)
